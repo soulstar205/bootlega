92321779d5a94eda2e5de65a57fdb7486b95f8f1
soulstar205 <soulstar205@gmail.com>
2023-03-18 12:18:12 +0100

    sitemap update

---
+++ src/components/sitemap/sitemap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+ 
+export default (
+    <Routes>
+        <Route path='/' />
+        <Route path='/ai' />
+        <Route path='/contact' />
+        <Route path='/about' />
+    </Routes>
+);

59ee84dac636fd392cc0ea1695f9084c1c7dda10
soulstar205 <soulstar205@gmail.com>
2023-07-12 04:48:19 +0100

    correct ads

--- src/components/ads/ads.jsx
+++ src/components/ads/ads.jsx
@@ -6,23 +6,22 @@ const AdsHorizontal = () =>{
     return(
         <div className='horizontal-ads-container'>
             <div className="ad-container-mobile">
-                <iframe
-                    title="Ad"
-                    src="https://files.webmoney.ru/files/fD7JVtEB/inline"
-                    width={468}
-                    height={60}
-                ></iframe>
-                <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'></a>
+                <a href="https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19" >
+                    
+                    <img src="https://files.webmoney.ru/files/fD7JVtEB/inline"
+                    alt=""
+                    style={{height: "60px", width : "468px"}} />
+                </a>
+               
             </div>
 
             <div className="ad-container-desktop">
-                <iframe
-                    title="Ad"
-                    src="https://files.webmoney.ru/files/dKdpetsg/inline"
-                    width={720}
-                    height={90}
-                ></iframe>
-                <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'></a>
+                <a href="https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19">
+                    <img src="https://files.webmoney.ru/files/dKdpetsg/inline" a
+                    alt="" 
+                    style={{height: "90px", width : "720px"}}/>
+                </a>
+                
             </div>
         </div>
     )
--- src/components/ads/verticals.jsx
+++ src/components/ads/verticals.jsx
@@ -9,13 +9,18 @@ export const Verticals =()=>{
 
     return(
         <div className="vertical-container">
-            <iframe
-                title="Ad"
-                src="https://files.webmoney.ru/files/bSpSsZsr/inline"
-                width={240}
-                height={400}
-            ></iframe>
-            <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'></a>
+            <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'>
+
+            <img src="https://files.webmoney.ru/files/bSpSsZsr/inline"
+                    alt=""
+                    style={{height: "400px", width : "240px"}} />
+                {/* <iframe
+                    title="Ad"
+                    src="https://files.webmoney.ru/files/bSpSsZsr/inline"
+                    width={240}
+                    height={400}
+                ></iframe> */}
+            </a>
          
         </div>
     )

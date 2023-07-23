6c28d52c15950b7d9962ee83ba559e2b9125d4b7
soulstar205 <soulstar205@gmail.com>
2023-07-23 16:01:57 +0100

    new ads

--- src/components/ads/ads.jsx
+++ src/components/ads/ads.jsx
@@ -6,15 +6,15 @@ const AdsHorizontal = () =>{
     return(
         <div className='horizontal-ads-container'>
             <div className="ad-container-mobile">
-                <a href="https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19" >
+            <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'>
                     
-                    <img src="https://files.webmoney.ru/files/fD7JVtEB/inline"
+                    <img src='https://files.webmoney.ru/files/a7ozSLv5/inline'
                     alt=""
-                    style={{height: "60px", width : "468px"}} />
+                    style={{height: "200px", width : "200px"}} />
                 </a>
                
             </div>
-
+            <a href='https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19'><img src='https://files.webmoney.ru/files/a7ozSLv5/inline'/></a>
             <div className="ad-container-desktop">
                 <a href="https://cashbox.money/r/ac0d1d9d65c34c069cc64dcc67d86c19">
                     <img src="https://files.webmoney.ru/files/dKdpetsg/inline" a
